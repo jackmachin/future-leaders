b9589621de38fc5a1060ba3c913d96df72cdc895
jackmachin <jack.machin125@gmail.com>
2015-08-12 16:38:58 +0100

    Logo

--- header.php
+++ header.php
@@ -55,7 +55,7 @@
             <div id="inner-header" class="container">
                 <div class="logo-container">
                     <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-                    <h1 class="site-title"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1>
+                    <a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo get_template_directory_uri(); ?>/library/images/logo.png" alt="<?php bloginfo('name'); ?>" class="logo"></a>
 
                     <?php // if you'd like to use the site description you can un-comment it below ?>
                     <?php // bloginfo('description'); ?>
